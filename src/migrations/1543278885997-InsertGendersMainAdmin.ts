802486d24bae0ce80c000ed751510909e9c4e730
Rafael Pascual <rpascual0812@gmail.com>
2022-09-10 14:00:04 +0800

    Added migrations

--- src/migrations/1543278885997-InsertGendersMainAdmin.ts
+++ src/migrations/1543278885997-InsertGendersMainAdmin.ts
@@ -1,16 +1,18 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InsertCompanyMainAdmin1648247817895 implements MigrationInterface {
+export class InsertGendersMainAdmin1643278885997 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            insert into companies (name, created_by) values ('Template Inc.', 1);
+            insert into genders (name) values ('Male');
+            insert into genders (name) values ('Female');
+            insert into genders (name) values ('Prefer not to say');
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            delete from accounts where name = 'Template Inc.';
+            delete from genders;
         `);
     }
 
