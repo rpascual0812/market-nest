13693c71cd13f55f55022e57c0227273e017297d
Rafael Pascual <rpascual0812@gmail.com>
2022-09-16 19:07:06 +0800

    Added products entity

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,50 @@
+import { User } from 'src/users/entities/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, Unique, JoinColumn, ManyToOne, OneToMany, Double } from 'typeorm';
+
+@Entity({ name: 'products' })
+@Unique(['uuid'])
+export class Product {
+    @PrimaryGeneratedColumn()
+    pk: number;
+
+    @Column({ type: 'text', nullable: false })
+    uuid: string;
+
+    @Column({ name: 'user_pk', nullable: false })
+    user_pk: number;
+
+    @Column({ type: 'text', nullable: false })
+    type: string;
+
+    @Column({ type: 'text', nullable: false })
+    name: string;
+
+    @Column({ type: 'int', nullable: false })
+    quantity: number;
+
+    @Column({ type: 'text', nullable: false })
+    measurement: string;
+
+    @Column({ type: 'int', nullable: false })
+    price_from: number;
+
+    @Column({ type: 'int', nullable: true })
+    price_to: number;
+
+    @Column({ type: 'text', nullable: false })
+    currency: string;
+
+    @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+    date_created: Date;
+
+    @Column({ default: false })
+    archived: boolean;
+
+    /**
+     * Relationship
+     */
+
+    @ManyToOne(type => User, user => user.ticket, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    @JoinColumn({ name: 'user_pk' })
+    user: User;
+}
---
+++ src/products/products.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, Response, UseGuards, HttpStatus, NotFoundException } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async create(@Body() body: any, @Request() req: any, @Response() res: any) {
+        const product = await this.productsService.create(body, req.user);
+        return res.status(product.status ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR).json(product);
+    }
+}
