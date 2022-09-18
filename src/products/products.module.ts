e82259ee227df067f2f9abc260752c7411ccf783
Rafael Pascual <rpascual0812@gmail.com>
2022-09-18 12:46:24 +0800

    Add products update

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { DatabaseModule } from 'src/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 
 @Module({
-  controllers: [ProductsController],
-  providers: [ProductsService]
+    imports: [
+        DatabaseModule,
+        TypeOrmModule.forFeature([Product]),
+    ],
+    controllers: [ProductsController],
+    providers: [ProductsService]
 })
-export class ProductsModule {}
+export class ProductsModule { }
