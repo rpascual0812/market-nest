7602ee54dfb85bc5fa79e1839cb6241c7924e412
Rafael Pascual <rpascual0812@gmail.com>
2022-10-04 21:13:19 +0800

    Fixed unique user address

--- src/users/user-addresses/entities/user-address.entity.ts
+++ src/users/user-addresses/entities/user-address.entity.ts
@@ -2,7 +2,7 @@ import { User } from 'src/users/entities/user.entity';
 import { Entity, Column, PrimaryGeneratedColumn, Unique, JoinColumn, ManyToOne, OneToOne, OneToMany, ManyToMany, JoinTable } from 'typeorm';
 
 @Entity({ name: 'user_addresses' })
-@Unique(['type', 'address'])
+@Unique(['type', 'address', 'user_pk'])
 export class UserAddress {
     @PrimaryGeneratedColumn()
     pk: number;
