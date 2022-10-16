49016f021bbca57231eea73b14566d932f527d6d
Rafael Aurelio Pascual <rpascual0812@gmail.com>
2022-10-16 20:06:56 +0800

    Added rating fetch and update

--- src/products/entities/product-ratings.entity.ts
+++ src/products/entities/product-ratings.entity.ts
@@ -1,10 +1,10 @@
 import { Product } from 'src/products/entities/product.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Entity, Column, PrimaryGeneratedColumn, Unique, JoinColumn, ManyToOne, OneToOne, OneToMany, ManyToMany, JoinTable } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Unique, JoinColumn, ManyToOne, OneToOne, OneToMany, ManyToMany, JoinTable, BaseEntity } from 'typeorm';
 
 @Entity({ name: 'product_ratings' })
 @Unique(['product_pk', 'user_pk'])
-export class ProductRating {
+export class ProductRating extends BaseEntity {
     @PrimaryGeneratedColumn()
     pk: number;
 
@@ -19,6 +19,9 @@ export class ProductRating {
 
     @Column({ type: 'text', nullable: true })
     message: String;
+
+    @Column({ default: false })
+    anonymous: boolean;
 
     @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
     date_created: Date;
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -15,11 +15,96 @@ export class ProductsController {
 
     @Get()
     async findAll(@Request() req: any) {
-        const data = await this.productsService.findAll(req.user, req.query);
-        if (data) {
-            return data;
-        }
+        const products = await this.productsService.findAll(req.user, req.query);
+        if (products) {
+            const pks = products[0].map(({ pk }) => pk);
 
-        throw new InternalServerErrorException();
+            const documents = await this.productsService.getProductDocuments(pks, req.query);
+            const ratings = await this.productsService.getProductRatings(pks, req.query);
+            const totalRatings = await this.productsService.getProductTotalRatings(pks);
+
+            products[0].forEach(product => {
+                if (!product.hasOwnProperty('product_documents')) {
+                    product['product_documents'] = [];
+                }
+                // Append product documents
+                if (documents) {
+                    documents[0].forEach(document => {
+                        if (product.pk == document.product_pk) {
+                            product['product_documents'].push(document);
+                        }
+                    });
+                }
+
+                if (!product.hasOwnProperty('product_ratings')) {
+                    product['product_ratings'] = [];
+                }
+                // Append product ratings
+                if (ratings) {
+                    ratings[0].forEach(rating => {
+                        if (product.pk == rating.product_pk) {
+                            product['product_ratings'].push(rating);
+                        }
+                    });
+                }
+
+                if (!product.hasOwnProperty('product_rating_total')) {
+                    product['product_rating_total'] = 0;
+                }
+                if (!product.hasOwnProperty('product_rating_count')) {
+                    product['product_rating_count'] = 0;
+                }
+                // Append product rating total
+                if (totalRatings['raw']) {
+                    totalRatings['raw'].forEach(rating => {
+                        if (product.pk == rating.product_pk) {
+                            product['product_rating_count'] = rating.count;
+                            product['product_rating_total'] = rating.total / rating.count;
+                        }
+                    });
+                }
+            });
+
+            return {
+                status: true,
+                data: products[0],
+                total: products[1]
+            }
+            // products[0].forEach(async (product, i) => {
+            //     const documents = await this.productsService.getProductDocuments(product['pk'], req.query);
+            //     products[0][i]['document'] = documents;
+            //     newProducts.push(product);
+
+            //     products['data'] = newProducts;
+            //     return products;
+            // });
+        }
+        else {
+        }
+        // console.log('products', newProducts);
+
+
+        // throw new InternalServerErrorException();
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('rating')
+    async findOneRatingPerUser(@Request() req: any, @Response() res: any) {
+        const data = await this.productsService.findOneRatingPerUser(req.query, req.user);
+        if (data) {
+            return res.status(HttpStatus.OK).json({ status: 'success', data: data });
+        }
+        return res.status(HttpStatus.FORBIDDEN).json({ status: 'failed' });
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('rating')
+    async createRating(@Body() body: any, @Request() req: any, @Response() res: any) {
+        const data = await this.productsService.createRating(body, req.user);
+        if (data) {
+            return res.status(HttpStatus.OK).json({ status: 'success', data: data });
+        }
+        return res.status(HttpStatus.FORBIDDEN).json({ status: 'failed' });
+        // return res.status(product.status ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR).json(product);
     }
 }
