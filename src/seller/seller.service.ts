50d86620283f377338867eb644bb4adf1c69d49f
Rafael Pascual <rpascual0812@gmail.com>
2022-10-29 18:08:30 +0800

    Added save producer

--- src/seller/seller.service.ts
+++ src/seller/seller.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSellerDto } from './dto/create-seller.dto';
-import { UpdateSellerDto } from './dto/update-seller.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import { Seller } from './entities/seller.entity';
 
 @Injectable()
 export class SellerService {
-  create(createSellerDto: CreateSellerDto) {
-    return 'This action adds a new seller';
-  }
+    constructor(
+        @InjectRepository(Seller)
+        private SellerRepository: Repository<Seller>,
+    ) { }
 
-  findAll() {
-    return `This action returns all seller`;
-  }
+    create(data, user) {
+        console.log(data);
+        const obj: any = {
+            uuid: uuidv4(),
+            user_pk: user.pk,
+            mobile_number: user.mobile_number,
+        }
 
-  findOne(id: number) {
-    return `This action returns a #${id} seller`;
-  }
+        const seller = this.SellerRepository.create(obj);
+        return this.SellerRepository.save(seller);
+    }
 
-  update(id: number, updateSellerDto: UpdateSellerDto) {
-    return `This action updates a #${id} seller`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} seller`;
-  }
+    findAll() {
+        return `This action returns all seller`;
+    }
 }
