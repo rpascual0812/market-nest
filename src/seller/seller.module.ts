194402108263ef66d5365e455bd821ac1f4794c6
Rafael Pascual <rpascual0812@gmail.com>
2022-11-08 21:06:58 +0800

    Fetch product by seller

--- src/seller/seller.module.ts
+++ src/seller/seller.module.ts
@@ -6,13 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Seller } from './entities/seller.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { ProductsService } from 'src/products/products.service';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { UserDocument } from 'src/users/entities/user-document.entity';
 
 @Module({
     imports: [
         DatabaseModule,
-        TypeOrmModule.forFeature([Seller, Product]),
+        TypeOrmModule.forFeature([Seller, Product, User, UserDocument]),
     ],
     controllers: [SellerController],
-    providers: [SellerService, ProductsService]
+    providers: [SellerService, ProductsService, UsersService]
 })
 export class SellerModule { }
