8ddab0949624b84f6840ebc865b803ad13bea5bd
Rafael Pascual <rpascual0812@gmail.com>
2023-05-30 22:21:47 +0800

    Added province delete

--- src/provinces/provinces.controller.ts
+++ src/provinces/provinces.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, UseGuards } from '@nestjs/common';
 import { ProvincesService } from './provinces.service';
 import { CreateProvinceDto } from './dto/create-province.dto';
 import { UpdateProvinceDto } from './dto/update-province.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('provinces')
 export class ProvincesController {
@@ -10,5 +11,17 @@ export class ProvincesController {
     @Get()
     findAll(@Request() req: any) {
         return this.provincesService.findAll(req.query);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('update')
+    async update(@Request() req: any, @Body() body: any) {
+        return await this.provincesService.update(body, req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Delete(':code')
+    async delete(@Param('code') code: number, @Request() req: any, @Body() body: any) {
+        return await this.provincesService.delete(code, req.user);
     }
 }
--- src/provinces/provinces.service.ts
+++ src/provinces/provinces.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { getRepository } from 'typeorm';
+import { Log } from 'src/logs/entities/log.entity';
+import { getConnection, getRepository } from 'typeorm';
 import { Province } from './entities/province.entity';
+import { DateTime } from "luxon";
 
 @Injectable()
 export class ProvincesService {
 
     async findAll(filters: any) {
+        console.log('filters', filters);
         try {
             const users = await getRepository(Province)
                 .createQueryBuilder('provinces')
-                // .select('provinces')
-                // .leftJoinAndSelect("provinces.country", "countries")
+                .andWhere(
+                    filters.hasOwnProperty('keyword') && filters.keyword != '' ?
+                        "(name ILIKE :keyword or province_code::text ILIKE :keyword)" :
+                        '1=1', { keyword: `%${filters.keyword}%` }
+                )
+                .andWhere("archived = :archived", { archived: `${filters && filters.hasOwnProperty('archived') ? filters.archived : false}` })
                 .skip(filters.skip)
                 .take(filters.take)
                 .orderBy('provinces.name')
@@ -29,5 +36,78 @@ export class ProvincesService {
                 status: false
             }
         }
+    }
+
+    async update(data: any, user: any) {
+        // console.log('updating user', data);
+        const queryRunner = getConnection().createQueryRunner();
+        await queryRunner.connect();
+
+        try {
+            return await queryRunner.manager.transaction(
+                async (EntityManager) => {
+                    const province = await EntityManager.findOne(Province, data.pk);
+                    province.province_code = data.province_code;
+                    province.name = data.name;
+                    const updatedProvince = await EntityManager.save(province);
+
+                    // LOGS
+                    const log = new Log();
+                    log.model = 'provinces';
+                    log.model_pk = data.pk;
+                    log.details = JSON.stringify({
+                        province_code: data.province_code,
+                        name: data.name,
+                        user_pk: user.pk,
+                        date_created: DateTime.now
+                    });
+                    log.user_pk = user.pk;
+                    await EntityManager.save(log);
+
+                    return { status: true, data: updatedProvince };
+                }
+            );
+        } catch (err) {
+            console.log(err);
+            return { status: false, code: err.code };
+        } finally {
+            await queryRunner.release();
+        }
+
+    }
+
+    async delete(code: any, user: any) {
+        const queryRunner = getConnection().createQueryRunner();
+        await queryRunner.connect();
+
+        try {
+            return await queryRunner.manager.transaction(
+                async (EntityManager) => {
+                    const province = await EntityManager.findOne(Province, code);
+                    province.archived = true;
+                    const updatedProvince = await EntityManager.save(province);
+
+                    // LOGS
+                    const log = new Log();
+                    log.model = 'products';
+                    log.model_pk = code;
+                    log.details = JSON.stringify({
+                        user_pk: user.pk,
+                        status: 'deleted',
+                        date_created: DateTime.now
+                    });
+                    log.user_pk = user.pk;
+                    await EntityManager.save(log);
+
+                    return { status: true, data: updatedProvince };
+                }
+            );
+        } catch (err) {
+            console.log(err);
+            return { status: false, code: err.code };
+        } finally {
+            await queryRunner.release();
+        }
+
     }
 }
