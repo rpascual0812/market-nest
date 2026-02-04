eb8121c1a81cf4bb9a389e82299ffed4c95a3d69
Rafael Pascual <rpascual0812@gmail.com>
2026-02-04 22:05:56 +0800

    Fixed search images

--- src/utilities/generate-s3-path.utils.ts
+++ src/utilities/generate-s3-path.utils.ts
@@ -1,4 +1,4 @@
 export const generatePath = (path: any, callback: any) => {
-    const complete_path: string = 'https://' + process.env.AWS_S3_BUCKET_NAME + '.s3.' + process.env.AWS_S3_REGION + '.amazonaws.com/' + path;
+    const complete_path: string = path.startsWith('https') ? path : 'https://' + process.env.AWS_S3_BUCKET_NAME + '.s3.' + process.env.AWS_S3_REGION + '.amazonaws.com/' + path;
     callback(complete_path);
 }
